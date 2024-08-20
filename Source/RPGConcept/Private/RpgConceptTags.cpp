14f60f5751527d99595e3282f6db2d5fe9131fc6
Paul Polbitsev <misterclass2909@yandex.ru>
2024-08-20 10:54:16 +0300

    Define input native gameplay tags

---
+++ Source/RPGConcept/Private/RpgConceptTags.cpp
@@ -0,0 +1,11 @@
+// Paul Polbitsev (@evilowl). All Rights Reserved
+
+
+#include "RpgConceptTags.h"
+
+namespace RPGGameplayTags
+{
+	/** Input Tags **/
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Move, "InputTag.Move")
+	UE_DEFINE_GAMEPLAY_TAG(InputTag_Look, "InputTag.Look")
+}
---
+++ Source/RPGConcept/Public/RpgConceptTags.h
@@ -0,0 +1,12 @@
+// Paul Polbitsev (@evilowl). All Rights Reserved
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace RPGGameplayTags
+{
+	/** Input Tags **/
+	RPGCONCEPT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Move)
+	RPGCONCEPT_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(InputTag_Look)
+}
